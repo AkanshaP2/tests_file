52d1109ff77751eb02e08704fc377b64e1775cf9
Akansha Pandey <127675854+AkanshaP2@users.noreply.github.com>
2024-05-24 16:45:02 +0530

    Create python_function.py

---
+++ python_function.py
@@ -0,0 +1,14 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+# Now let's use the function
+# Example 1:
+len = 5
+wid = 2
+area1 = calculate_rectangle_area(len, wid)
+print(f"Area of the rectangle with length {len} and width {wid}: {area1}")
+
+# Example 2:
+area2 = calculate_rectangle_area(9, 3)
+print(“area2 is: ”, area2)
